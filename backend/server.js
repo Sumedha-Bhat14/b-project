72044bf08e723adc9f121a3da2d8a06aa769265d
Sumedha-Bhat14 <sparklesoul067@gmail.com>
2026-03-13 21:16:48 -1200

    prjct Bcknd

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", (req, res)=>{
+//     res.send("backend is running");
+// });
+// app.listen(4000,()=>{
+//     console.log("server running on port 4000");
+// });
+
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', (req,res)=>{
+     const {name,email,password,role} = req.body;
+    console.log("received data");
+    console.log(name,email,password,role);
+    res.json({message: "Registerd Successfull"});
+});
+ app.listen(3000, () => {
+     console.log("Server is running on port 3000");
+}); 
+
+
+
+app.post('/login', (req,res)=>{
+    const {email,password} = req.body;
+   console.log("received data");
+   console.log(email,password);
+   res.json({message: "Login Successfull"});
+} ); 
+
+
+app.get('/jobs', (req, res) => {
+    const jobs = [
+        { title: "Frontend Developer", company: "ABCD Tech", location: "Bangalore" },
+        { title: "Backend Developer", company: "XYZ Ltd", location: "Hyderabad" },
+        { title: "Full Stack Developer", company: "TechSoft", location: "Chennai" }
+    ];
+
+    res.json(jobs);
+});
